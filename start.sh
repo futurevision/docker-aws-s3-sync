fc561e33de90a3806a8b306398fd31aabd297b63
Jarno Henneman <jarno.henneman@bertelsmann.de>
2018-03-09 20:09:39 +0800

    Make ENV variables optional, so Role on container can be used

--- start.sh
+++ start.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-export AWS_ACCESS_KEY_ID=$KEY
-export AWS_SECRET_ACCESS_KEY=$SECRET
-export AWS_DEFAULT_REGION=$REGION
+if [[ $KEY ]]; then
+  echo "Set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION"
+
+  export AWS_ACCESS_KEY_ID=$KEY
+  export AWS_SECRET_ACCESS_KEY=$SECRET
+  export AWS_DEFAULT_REGION=$REGION
+fi
 
 if [[ "$1" == 'now' ]]; then
     exec /sync.sh
