b51e286eca41bf9d6f2c8b8028ee548c7bc2dbb9
Jarno Henneman <jarno.henneman@bertelsmann.de>
2018-03-09 22:28:21 +0800

    .

--- start.sh
+++ start.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-if [[ $KEY ]]; then
+if [[ -z "$KEY" ]] || [[ -z "$SECRET" ]] || [[ -z "$REGION" ]]; then
   echo "Set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION"
 
   export AWS_ACCESS_KEY_ID=$KEY
   export AWS_SECRET_ACCESS_KEY=$SECRET
   export AWS_DEFAULT_REGION=$REGION
+else
+  echo "Missing one or more ENV variables, using container role"
 fi
 
 if [[ "$1" == 'now' ]]; then
